e422bf1bb8ff6a33f8cc619a478eeb35f53ed1f1
kw7oe <choongkwern@hotmail.com>
2017-03-26 16:40:15 +0800

    Edit environment.rb

--- environment.rb
+++ environment.rb
@@ -1,13 +1,3 @@
-configure :production, :development do 
-  db = URI.parse(ENV["DATABASE_URL"] || "postgres://localhost/kai")
+require 'active_record'
 
-  ActiveRecord::Base.establish_connection(
-    :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
-    :host => db.host,
-    :username => db.user,
-    :password => db.password,
-    :databse => db.path[-1..1],
-    :encoding => "utf-8"
-    )
-
-end
+ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/kai')
